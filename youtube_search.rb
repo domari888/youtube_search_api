8afc93fa29716ca6fb5656a58c8e388f634678e6
damari <motokin128@gmail.com>
2021-07-04 15:50:10 +0900

    API キーを設定

--- youtube_search.rb
+++ youtube_search.rb
@@ -1,1 +1,6 @@
 require "dotenv/load"
+require "google/apis/youtube_v3"
+
+# 動画が取得できる機能を持ったYouTubeServiceオブジェクトを作成
+youtube = Google::Apis::YoutubeV3::YouTubeService.new
+youtube.key = ENV["GOOGLE_API_KEY"]
